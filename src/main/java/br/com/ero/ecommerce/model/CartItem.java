ff20f8c02d848ea62e12b58c635ca647ee87d435
Lucas Lima <lucaserolima@gmail.com>
2024-08-07 15:43:05 -0400

    Created the entities and their relationships for the ecommerce application

---
+++ src/main/java/br/com/ero/ecommerce/model/CartItem.java
@@ -0,0 +1,25 @@
+package br.com.ero.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "cart_id", nullable = false)
+  private Cart cart;
+  @ManyToOne
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
+  private Integer quantity;
+
+}
---
+++ src/main/java/br/com/ero/ecommerce/model/Order.java
@@ -0,0 +1,37 @@
+package br.com.ero.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+  private String address;
+  private String phoneNumber;
+  private OrderStatus status;
+
+  public enum OrderStatus {
+    PREPARING,
+    DELIVERING,
+    DELIVERED,
+    CANCELED
+  }
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<OrderItem> items = new ArrayList<>();
+}
