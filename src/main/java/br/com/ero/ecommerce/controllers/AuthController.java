f80afd54b648d810b5d3e6ca836ce58572b5c4a0
Lucas Lima <lucaserolima@gmail.com>
2024-08-15 18:45:08 -0400

    Created the end point for change password from user

--- src/main/java/br/com/ero/ecommerce/controllers/AuthController.java
+++ src/main/java/br/com/ero/ecommerce/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package br.com.ero.ecommerce.controllers;
 
+import br.com.ero.ecommerce.dto.ChangePasswordRequest;
 import br.com.ero.ecommerce.dto.LoginRequest;
 import br.com.ero.ecommerce.model.User;
 import br.com.ero.ecommerce.service.JwtService;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,5 +41,13 @@ public class AuthController {
   @PostMapping("/register")
   public ResponseEntity<User> register(@RequestBody @Valid User user) {
     return ResponseEntity.ok(userService.registerUser(user));
+  }
+
+  @PostMapping("/change-password")
+  public ResponseEntity<?> changePassword(@RequestBody @Valid ChangePasswordRequest request) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String email = authentication.getName();
+    userService.changePassword(email, request);
+    return ResponseEntity.ok().body("Password changed");
   }
 }
