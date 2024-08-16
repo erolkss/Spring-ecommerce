571c6895b22d7a1d9ced38c48353e4d7468acb72
Lucas Lima <lucaserolima@gmail.com>
2024-08-16 16:10:08 -0400

    feat: Creating an endpoint to update a product

--- src/main/java/br/com/ero/ecommerce/controllers/ProductController.java
+++ src/main/java/br/com/ero/ecommerce/controllers/ProductController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,8 +20,20 @@ public class ProductController {
   private final ProductService productService;
 
   @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  @PreAuthorize("hasRole(ADMIN)")
-  public ResponseEntity<ProductDTO> createProduct(@RequestPart("product") @Valid ProductDTO productDTO, @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<ProductDTO> createProduct(
+          @RequestPart("product") @Valid ProductDTO productDTO,
+          @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
     return ResponseEntity.ok(productService.createProduct(productDTO, image));
+  }
+
+  @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<ProductDTO> updateProduct(
+          @PathVariable Long id,
+          @RequestPart("product") @Valid ProductDTO productDTO,
+          @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+
+    return ResponseEntity.ok(productService.updateProduct(id, productDTO, image));
   }
 }
