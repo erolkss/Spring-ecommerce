8765051ff1e8eb01b2405c2a79ddfac9bb10473f
Lucas Lima <lucaserolima@gmail.com>
2024-08-16 15:11:17 -0400

    fix: Fixed database connection error

--- src/main/java/br/com/ero/ecommerce/repositories/UserRepository.java
+++ src/main/java/br/com/ero/ecommerce/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package br.com.ero.ecommerce.repositories;
 
 import br.com.ero.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
