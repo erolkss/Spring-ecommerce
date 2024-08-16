fa33286059805f4d1a0bf7d106eb0c396a3e8383
Lucas Lima <lucaserolima@gmail.com>
2024-08-16 16:06:10 -0400

    feat: Created the first 'ProductController' endpoint for creating a new product

---
+++ src/main/java/br/com/ero/ecommerce/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package br.com.ero.ecommerce.controllers;
+
+import br.com.ero.ecommerce.dto.ProductDTO;
+import br.com.ero.ecommerce.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+  private final ProductService productService;
+
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PreAuthorize("hasRole(ADMIN)")
+  public ResponseEntity<ProductDTO> createProduct(@RequestPart("product") @Valid ProductDTO productDTO, @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+    return ResponseEntity.ok(productService.createProduct(productDTO, image));
+  }
+}
