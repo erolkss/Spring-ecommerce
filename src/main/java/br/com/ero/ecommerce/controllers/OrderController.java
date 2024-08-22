a9eaf637ff878e6a67d3d1d6dfe7b39130b2ccb1
Lucas Lima <lucaserolima@gmail.com>
2024-08-22 15:14:52 -0400

    feat: Created endpoint to return all requests, only admins can make this request

--- src/main/java/br/com/ero/ecommerce/controllers/OrderController.java
+++ src/main/java/br/com/ero/ecommerce/controllers/OrderController.java
@@ -8,10 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -31,4 +30,11 @@ public class OrderController {
     return ResponseEntity.ok(orderDTO);
   }
 
+  @GetMapping()
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<List<OrderDTO>> getAllOrders() {
+    List<OrderDTO> orders = orderService.getAllOrders();
+
+    return ResponseEntity.ok(orders);
+  }
 }
